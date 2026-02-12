47ede0537d9f122bda9817f28268a394201bd087
elias <eliasstieler@icloud.com>
2026-02-12 20:36:41 +0100

    refactor layout and styling for improved UI consistency
    fixes #1
    fixes #2

--- WelpenWache/Program.cs
+++ WelpenWache/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Negotiate;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Server.HttpSys;
 using MudBlazor.Services;
 using WelpenWache;
